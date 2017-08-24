0cb213829935b07f998d4c7e02a31c34d82e0f96
Jean-Francois Jung <pedagogie@itechsup.fr>
2017-08-24 17:10:21 +0200

    J'adore cet exercice. il est complet

---
+++ Exercice 2/exercice 2.ts
@@ -0,0 +1,10 @@
+/*
+Dans ce fichier, écrire un nombre arbitraire de fonctions.
+Ces fonctions doivent faire appel à chacun des types basiques
+de Typescript.
+Bien entendu, chaque fonction ne doit pas faire à tous les types,
+mais tous les types doivent être présent au moins une fois dans vos fonctions
+
+Vérifier la bonne compilation de vote code, inspecter le code produit et
+soumettre le résultat sur votre dépot github
+*/
