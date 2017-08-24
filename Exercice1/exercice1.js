743f08b9655d621e5155822cc75c7ace14fc15e4
Jean-Francois Jung <pedagogie@itechsup.fr>
2017-08-24 12:09:43 +0200

    Premier exercice

---
+++ Exercice1/exercice1.js
@@ -0,0 +1,19 @@
+/*
+Ce fichier est un fichier javascript standard
+
+Partie 1 : compilation
+
+1. renommez-le en exercice1.ts
+2. compilez-le avec la commande >tsc exercice1.ts
+3. observez le code javascript généré par transpilage
+
+Partie 2 : typage
+1. ajouter un typage par hinting sur l'argument de la fonction
+    le hinting d'argument s'écrit de la sorte : arg: type
+2. compiler
+3. observer le code compilé
+
+*/
+function saluer(nom) {
+    console.log('coucou ' + nom);
+}
