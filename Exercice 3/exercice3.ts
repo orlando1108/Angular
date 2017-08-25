3001cef26cf1805788107c39c6ea2c238be93209
Jean-Francois Jung <pedagogie@itechsup.fr>
2017-08-25 09:49:18 +0200

    Exo3 part 1 and 2

---
+++ Exercice 3/exercice3.ts
@@ -0,0 +1,17 @@
+/*
+Partie 1
+Créer une classe Personne avec deux attributs : nom et prenom
+et une methode permettant à la personne de se présenter au travers d'un
+message dans la console
+
+*/
+
+class personne
+
+/*
+Partie 2
+Créer une classe étudiant, héritant de la classe personne
+Ajout un attribut cursus sur l'étudiant prenant dans un énumération
+de votre création
+
+*/
