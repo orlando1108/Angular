54eb229bb545024c6e5a243fda1a46260bd1b107
Jean-Francois Jung <pedagogie@itechsup.fr>
2017-08-25 10:18:34 +0200

    Plus de clarté dans la consigne

--- Exercice 3/exercice3.ts
+++ Exercice 3/exercice3.ts
@@ -2,8 +2,10 @@
 Partie 1
 Créer une classe Personne avec deux attributs : nom et prenom
 et une methode permettant à la personne de se présenter au travers d'un
-message dans la console
+message dans la console.
 
+Vérifier la bonne compilation de vote code, inspecter le code produit et
+soumettre le résultat sur votre dépot github
 */
 
 class Personne
@@ -12,8 +14,12 @@ class Personne
 Partie 2
 Créer une classe étudiant, héritant de la classe personne
 Ajout un attribut cursus sur l'étudiant prenant dans un énumération
-de votre création
+de votre création.
+
+Vérifier la bonne compilation de vote code, inspecter le code produit et
+soumettre le résultat sur votre dépot github
 
 */
+enum ...
 
 class Etudiant
